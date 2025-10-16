1a8fd01d44d36f280f0e0d9f6176203fd70a3c69
yuvikadessai <yuvikadessai05@gmail.com>
2025-10-16 18:56:09 +0530

    updated file

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const routerLogin = require("./routesLogin");
 const routerRegister = require("./routesRegister");
 const routerContact = require("./routesContact"); 
-const session = require("express-session")
+const session = require("express-session");
+const routerPayment = require("./routesPayment");
+
 
 dotenv.config();
 const app = express();
@@ -13,17 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(session({
-    secret: "mySecretKey",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {maxAge: 24 * 60 * 60 * 1000}
-}))
-
 // Mount routers
 app.use("/login", routerLogin);
 app.use("/register", routerRegister);
-app.use("/contact", routerContact);
+app.use("/contact", routerContact)
+app.use("/", routerPayment); ;
 
 const PORT = 8000;
 app.listen(PORT, () => {
